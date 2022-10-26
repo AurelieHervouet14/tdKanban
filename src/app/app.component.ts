e0b35e82de8c65e95011fa0dfb7ff9cb9314c166
AurelieHervouet14 <104544099+AurelieHervouet14@users.noreply.github.com>
2022-10-26 20:11:49 +1100

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { KanbanComponent } from 'smart-webcomponents-angular/kanban';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'tdKanban';
+export class AppComponent implements AfterViewInit,  OnInit{
+  @ViewChild('kanban',
+   { read: KanbanComponent, static: false }
+   ) 
+   kanban!: KanbanComponent;
+
+  addNewColumn = true;
+
+  columns = [
+    { addNewButton: false }
+];
+
+  ngOnInit(): void {
+    // onInit code.
+  }
+
+  ngAfterViewInit(): void {
+    // afterViewInit code.
+    this.init();
+  }
+
+  init(): void {
+    // init code.
+  }
 }
